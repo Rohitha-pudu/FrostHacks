f2667b8a63dd2997ecbcf34d38dc50ba896ad380
Rohitha-pudu <116823590+Rohitha-pudu@users.noreply.github.com>
2024-01-21 17:21:11 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,27 +16,26 @@ export const Home2 = async () => {
 
   return (
     <>
-      <div className='container mb-20 pl-10 pr-10'>
-      <div className='image'></div>
-      <div className='text'>
-        <h1 className="head-txt">Unlock Smart Shopping with every price detail</h1>
-        <p className="body-txt">TrackWatch, your ultimate destination for seamless and insightful price tracking,<br></br> ensuring you never miss a deal again.</p>
-      </div>
-    </div>
- 
-    <p  className="home-text">Pick your shop <span>...</span></p>
-
-<div className="mt-5 pl-6">
-  <div className="mt-30">
+ <div className="ml-20 mt-20">
+  <div className="pl-50">
     <Image
             src="/assets/images/Amazon.png"
             alt="arrow-right"
             className='x'
-            width={270}
-            height={270}
+            width={220}
+            height={220}
            
             />
 </div>
+
+            <div  className='pl-20 pt-2'>
+            <Link href={`/amazon`}>
+             <button  type="button"   className="searchbar-btn">
+            Track</button> 
+            </Link>
+            
+           </div>
+  </div>  
 
             <div  className='flex pl-40'>
             <Link href={`/amazon`}>
